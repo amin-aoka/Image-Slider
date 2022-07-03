e8a56cfd41c8b656e765984ba2462c1aa0b29e89
AOKA <amin.aoka@gmail.com>
2022-07-03 22:37:09 +0430

    function complete

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let rightArrow = document.querySelector(".right")
 let leftArrow = document.querySelector(".left")
-let current = document.querySelector(".current")
 let slide = document.querySelector(".slide")
 
 rightArrow.addEventListener("click" ,function (){
-
+    let current = document.querySelector(".current")
     current.nextElementSibling.classList.add("current")
     current.classList.remove("current")
 
